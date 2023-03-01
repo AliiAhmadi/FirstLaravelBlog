c7b9a2ddae20db61807451d0c39c9b3cd1d0d2b5
aliiahmadi <aliahmadi.github@gmail.com>
2023-03-01 06:42:53 +0330

    migrations - model - controller

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,5 +3,27 @@
 
 
 @section('content')
-    index
+    <div class="container py-4">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <!-- Add text-center class -->
+                <h1>My Blog</h1>
+                <hr>
+                <!-- Display all posts here -->
+                @foreach ($posts as $post)
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p class="card-text">{{ $post->content }}</p>
+                            <!-- Edit, Delete, and Show buttons -->
+                            <a href="#" class="btn btn-primary">Edit</a>
+                            <a href="#" class="btn btn-danger">Delete</a>
+                            <a href="#" class="btn btn-secondary">Show</a>
+                        </div>
+                    </div>
+                @endforeach
+                <!-- Repeat above card div for each post -->
+            </div>
+        </div>
+    </div>
 @endsection
